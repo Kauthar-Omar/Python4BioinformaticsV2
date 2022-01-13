bbee292bd1c23d804e875949abad626b23c6c56c
Kauthar-Omar <kauthyomar@gmail.com>
2022-01-13 18:35:03 +0300

    updated notebooks

---
+++ Notebooks/gene_modules/gene_names.py
@@ -0,0 +1,20 @@
+'''
+This is me practising how to use modules.
+'''
+def gen_names(gene_file, gene_names):
+    '''
+    Takes input as chromosome file and extracts gene_names from it.
+    '''
+    with open(gene_file, 'r') as myfile:
+        with open(gene_names, "w") as outfile:
+            count = 0
+            flag = False
+            for line in myfile:
+                if line .startswith('______________'):
+                    flag = True
+                    count += 1
+                    continue
+                elif line.startswith('-'):
+                    flag = False
+                if flag and count ==2:
+                    outfile.write(line.split()[0] + '\n')
